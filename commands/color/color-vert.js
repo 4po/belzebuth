3724316bca60021d4cc2fb0fbeaaffa1a9196390
Younes Azem <66745853+4po@users.noreply.github.com>
2021-02-22 09:36:04 +0100

    Add files via upload

---
+++ commands/color/color-vert.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+
+exports.run = (client, message, args) => {
+    let arg = message.content.split(" ").slice(1);
+    let thingToEcho = arg.join(" ")
+    if (!args[0]) {
+        var error_one = new Discord.RichEmbed()
+            .setDescription("❌ Merci d'inclure un message pour effectuer cette commande.")
+            .setColor("#DD2E44")
+        message.channel.send(error_one)
+    }else {
+    var color = new Discord.RichEmbed()
+        .setDescription(thingToEcho)
+        .setColor("#00FF00")
+        .setFooter(`Envoyé par ${message.author.tag}`, message.author.avatarURL)
+    message.channel.send(color)
+    message.delete()
+}
+}
+
+module.exports.help = {
+    name: "color-vert"
+}
